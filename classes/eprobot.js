4c7a8485372b586c726845a7807861cd36b66f49
Christian Wehr <christian.wehr@gmx.de>
2019-05-27 19:24:19 +0200

    query method

--- classes/eprobot.js
+++ classes/eprobot.js
@@ -1,47 +1,14 @@
 class Eprobot {
 
     constructor(x_pos, y_pos) {
-        let options_body = {
+        this.body = Matter.Bodies.circle(x_pos, y_pos, 50, {
             isStatic: false,
-            frictionAir: 0,
             render: {
-                fillStyle: '#ff0000',
+                fillStyle: '#940000',
             }
-        }
-
-        let options_sensor = {
-            //density: 0.04, //default 0.001
-            friction: 0, //default 0.1
-            frictionAir: 0, //default 0.01
-            //restitution: 0.8, //default 0
-            render: {
-                fillStyle: '#1400f5',
-                //strokeStyle: 'black',
-                //lineWidth: 1
-                visible: true
-            },
-            chamfer: { radius: [0, 0, 90, 0] }
-        }
-
-        // -------------
-        let eprobots_radius = 50;
-        let eprobot_body = Matter.Bodies.circle(x_pos, y_pos, eprobots_radius, options_body);
-        eprobot_body.my_label = "Eprobot";
-        eprobot_body.my_parent = this;
-
-        let eprobot_sensor = Matter.Bodies.rectangle(x_pos+eprobots_radius, y_pos, eprobots_radius*2, eprobots_radius*2, options_sensor);
-        Matter.Body.rotate(eprobot_sensor, -Math.PI/4);
-        eprobot_sensor.isSensor = true;
-        eprobot_sensor.my_label = "Eprobot Sensor";
-        eprobot_sensor.my_parent = this;
-
-        let eprobot_compound = Matter.Body.create({
-            parts: [eprobot_body, eprobot_sensor]
         });
 
-        //Matter.Body.scale(eprobot_compound, 0.5, 0.5);
-        this.body = eprobot_compound;
-        // -------------
-
+        this.body.my_label = "Eprobot";
+        this.body.my_parent = this;
     }
 }
--- index.js
+++ index.js
@@ -1,6 +1,24 @@
 var WORLD_WIDTH = window.innerWidth; //window.screen.width; //1800;
 var WORLD_HEIGHT = window.innerHeight; //window.screen.height; //800
 
+var engine;
+var eprobot;
+
+function q(){
+    var all_bodies = Matter.Composite.allBodies(engine.world);
+
+    var raylength = 200;
+    var raywith = 50;
+    let pos_x = eprobot.body.position.x + (raylength/2)*Math.cos(eprobot.body.angle);
+    let pos_y = eprobot.body.position.y + (raylength/2)*Math.sin(eprobot.body.angle);
+    let ray = Matter.Bodies.rectangle(pos_x, pos_y, raylength, raywith, { angle: eprobot.body.angle });
+    //Matter.World.add(engine.world, ray);
+    //var verts = Matter.Vertices.create([{ x: -500, y: -500 }, { x: -500, y: 500 }, { x: 500, y: 500 }, { x: 500, y: -500 }], eprobot.body)
+    //var bounds = Matter.Bounds.create(verts);
+    var bodies_in_bounds = Matter.Query.region(all_bodies, ray.bounds);
+    return bodies_in_bounds.length;
+}
+
 window.onload = function() {
 
     // module aliases
@@ -12,7 +30,7 @@ window.onload = function() {
     var myCanvas = document.getElementById('world');
 
 	// create an engine
-	var engine = Engine.create();
+	engine = Engine.create();
 
     // turn gravity off
     engine.world.gravity.y = 0;
@@ -35,7 +53,7 @@ window.onload = function() {
     var energyobject = new Energy(WORLD_WIDTH/2, WORLD_HEIGHT/2);
     Matter.World.add(engine.world, energyobject.body);
 
-    var eprobot = new Eprobot(WORLD_WIDTH/2-200, WORLD_HEIGHT/2);
+    eprobot = new Eprobot(WORLD_WIDTH/2-200, WORLD_HEIGHT/2);
     Matter.World.add(engine.world, eprobot.body);
 
 	// run the engine
